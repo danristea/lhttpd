ac4e5d725984a72a2bb6e337e706801a41daa50d
danristea <danristea@icloud.com>
2022-11-02 10:16:52 -0700

    Add files via upload

--- time.c
+++ time.c
@@ -1,3 +1,31 @@
+/*
+BSD 2-Clause License
+
+Copyright (c) 2022, danristea
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #define _DEFAULT_SOURCE
 
 #ifdef __linux__
@@ -14,6 +42,7 @@
 #endif
 
 // Use clock_gettime in linux, clock_get_time in OS X.
+// courtesy of https://stackoverflow.com/questions/5167269/clock-gettime-alternative-in-mac-os-x
 void get_monotonic_time(struct timespec *ts){
 #ifdef __MACH__
     clock_serv_t cclock;
@@ -32,9 +61,9 @@ char *
 httpd_time(char *date, size_t datelen)
 {
     time_t now = time(NULL);
-    struct tm tm = *gmtime(&now);
-    strftime(date, datelen, "%a, %d %h %Y %T %Z", &tm);
-	  return date;
+    struct tm *tm = gmtime(&now);
+    strftime(date, datelen, "%a, %d %h %Y %T %Z", tm);
+    return date;
 }
 
 int
@@ -49,6 +78,7 @@ parse_date(const char *val, time_t *timestamp)
 }
 
 // Use clock_gettime in linux, clock_get_time in OS X.
+// courtesy of https://stackoverflow.com/questions/5167269/clock-gettime-alternative-in-mac-os-x
 void get_calendar_time(struct timespec *ts){
 #ifdef __MACH__
     clock_serv_t cclock;
