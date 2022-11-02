3d771f94c3e44125be538ac8139d63987ef0333a
danristea <danristea@icloud.com>
2022-11-02 15:56:27 -0700

    Add files via upload

--- httpd-aio.h
+++ httpd-aio.h
@@ -1,23 +1,48 @@
+/*
+BSD 2-Clause License
+
+Copyright (c) 2022, danristea
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #ifndef _HTTPD_AIO_
 #define _HTTPD_AIO_
 
 #include "event.h"
 #include "queue.h"
 
-//#define MAX_AIO (sysconf(_SC_AIO_MAX))
+
 #define MAX_AIO (_SC_AIO_MAX)
 #define MAX_LISTIO (sysconf(_SC_AIO_LISTIO_MAX))
 
 typedef struct aio_data {
-    FILE* f;
     int fd;
     char *buf;
-    int len;
-    int pos;
-    int flen;
-    int ready;
+    size_t len;
+    long pos;
+    off_t flen;
     void *ctx;
-    LIST_ENTRY(aio_data) next;
 } aio_data;
 
 struct async_io {
@@ -26,21 +51,18 @@ struct async_io {
     int ac;
     int nc;
     int wait;
+    int ready;
     void *nlist;
     void *alist;
+    void *tlist[MAX_AIO];
 };
 
-
 struct async_io *aio_init(int pipe[2]);
-//void schedule_aio_read(struct async_io *aio, struct aio_data *aio_d, void *ctx, int fd, char *buf, int len, int pos);
-//void schedule_aio_read(struct async_io *aio, struct aio_data *aio_d, void *ctx, int fd, char *buf, int len, int pos);
-void schedule_aio_read(struct async_io *aio, struct aio_data *aio_d);
-void schedule_aio_write(struct async_io *aio, struct aio_data *aio_d);
-void lh_aio_schedule(struct async_io *aio, void *ctx);
-//int lh_aio_reap(void *alist, struct aio_data *aio_d[], int ac);
-int lh_aio_reap(struct async_io *aio, struct aio_data *aio_d[], int ac);
 
-//#define AIO_SIGD() aio_sigd()
+void schedule_aio(struct async_io *, struct aio_data *, short);
+void lh_aio_schedule(struct async_io *, void *);
+int lh_aio_reap(struct async_io *, struct aio_data **, int);
+
 #define AIO_INIT(pipe) aio_init(pipe)
 
 #endif
