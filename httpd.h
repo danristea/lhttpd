956b7b7182911ef08f465f7bc1e7d6582b0dfbb1
danristea <danristea@icloud.com>
2022-11-04 09:35:41 -0700

    Add files via upload

--- httpd.h
+++ httpd.h
@@ -1,3 +1,31 @@
+/*
+BSD 2-Clause License
+
+Copyright (c) 2022, danristea
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #ifndef _HTTPD_H_
 #define _HTTPD_H_ 1
 
@@ -19,21 +47,14 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-//#include "queue.h"
 #include "lua.h"
 #include "event.h"
 #include "httpd-aio.h"
 #include "http.h"
-
-
-//#include "lualib.h"
 #include "lauxlib.h"
 #include "bearssl.h"
 #include "brssl.h"
-#include <hpack.h>
-
-
-//#include "hpack.h"
+#include "hpack.h"
 
 #define BUFFER_SIZE 4096
 #define H2_HEADER_SIZE 9
@@ -48,32 +69,13 @@
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #endif
 
-enum conn_state {
-    CS_INIT = (0x0),
-    CS_RECV = (0x1),
-    CS_SEND = (0x2),
-    CS_WAIT = (0x4),
-    CS_CLOSE = (0x8),
-    CS_ERROR = (0x10)
-};
-
-struct http_hfield {
-   const char* name;
-   const char* value;
-};
-
-//enum strm_state {
-//    SS_INIT, SS_RECV, SS_HEAD, SS_SEND, SS_WAIT, SS_ERROR
-//};
-
-enum strm_state {
-    SS_INIT = (0x0),
-    SS_WAIT = (0x1),
-    SS_RECV = (0x2),
-    SS_HEAD = (0x4),
-    SS_SEND = (0x8),
-    SS_CLOSE = (0x10),
-    SS_ERROR = (0x100)
+enum io_state {
+    IO_NONE = (0x0),
+    IO_RECV = (0x1),
+    IO_SEND = (0x2),
+    IO_CLOSE = (0x6),
+    IO_ERROR = (0x8),
+    IO_WAIT = (0x10)
 };
 
 // BR_SSL_CLOSED    0x0001
@@ -105,60 +107,39 @@ typedef struct config {
     void *tls_config;
     char *cert_file;
     char *pkey_file;
-    char *mime;
     int debug;
     int fg;
     SIMPLEQ_HEAD(l_map, lua_map) l_map;
 } config;
 
 struct h2_settings {
-	  uint32_t header_table_size;
-	  uint32_t enable_push;
-	  uint32_t max_concurrent_streams;
-	  uint32_t window_size;
-	  uint32_t max_frame_size;
-	  uint32_t max_header_list_size;
+    uint32_t header_table_size;
+    uint32_t enable_push;
+    uint32_t max_concurrent_streams;
+    uint32_t window_size;
+    uint32_t max_frame_size;
+    uint32_t max_header_list_size;
 };
-
-/*
-struct h2_frame {
-	  uint8_t	f_len[3];
-	  uint8_t	f_typ;
-	  uint8_t	f_flg;
-	  uint8_t	f_sid[4];
-};
-*/
-
 
 struct h2_frame {
-	  uint32_t f_len; //size_t f_len;
-	  uint8_t f_typ;
-	  uint8_t f_flg;
-	  uint32_t f_sid;
-};
-
-struct hp {
-    char* data;
-    size_t offset;
-};
-
-struct frame_header {
-    uint8_t data[H2_HEADER_SIZE];
+    uint32_t f_len;
+    uint8_t f_typ;
+    uint8_t f_flg;
+    uint32_t f_sid;
 };
 
 struct frame_settings {
-    struct frame_header fh;
+    uint8_t fh[H2_HEADER_SIZE];
     struct {
         uint16_t id;
         uint32_t val;
     } fields[];
 };
 
-
 typedef struct lua_handler {
-	  const char	*name;
-	  int		 ref;
-	  SIMPLEQ_ENTRY(lua_handler) next;
+    const char	*name;
+    int		 ref;
+    SIMPLEQ_ENTRY(lua_handler) next;
 } lua_handler;
 
 typedef struct lua_state_map {
@@ -169,59 +150,26 @@ typedef struct lua_state_map {
     SIMPLEQ_ENTRY(lua_state_map) next;
 } lua_state_map;
 
-/*
-struct cb_data {
-    short typ;
-    union {
-        struct edata e_dat;
-        struct adata a_dat;
-    } dt;
-};
-*/
 
-/*
-typedef struct strm_file {
-    FILE* F;
-    off_t len;
-    off_t idx;
-    char* buf;
-    SIMPLEQ_ENTRY(strm_file) next;
-} strm_file;
-*/
+typedef struct stream {
+    int head;
 
-struct stream {
-    //struct aio_data aio_d;
-    LIST_HEAD(aio_d, aio_data) aio_d;
     struct edata ev;
-    enum strm_state ss;
+    enum io_state ss;
 
-    lua_State* L; //take it out maybe
+    lua_State* L;
     lua_State* T;
-    struct connect* conn;
+    struct connection *conn;
 
-    int id;
-    int pri;
-    int fd;
-    //FILE** f;
-    luaL_Buffer lb;
     int lua_status;
 
-    //unsigned char* buf;
-    //int lwm;
-    //int io_hwm;
-    //int io_lwm;
     size_t io_len;
     size_t io_idx;
-    size_t io_pos; //new
     char *io_buf;
-    int rb;
-    int rd;
-    int fsio;
-    int not_found;
 
     //HTTP/2
     enum h2_stream_state h2_ss;
-    enum h2_error_codes h2_err;
+    enum h2_error h2_error;
     uint32_t h2_sid;
     int h2_dep;
     int h2_wgt;
@@ -234,26 +182,10 @@ struct stream {
     char *path;       //:path (HTTP/2)
 
     enum http_method http_method;
-    char* content;
-    char* content_type;
-    char* host;
-    char* referrer;
-    char* range;
-    char* modified_since;
-    char* accept_encoding;
-    char* user_agent;
-    char* accept;
-    long content_len;
-    long tlen;
     int http_status;
 
-    struct header* head;
-
-    struct stream* next;
-    struct stream* prev;
-    int sh;
-//    SIMPLEQ_HEAD(s_file, strm_file) s_file;
-};
+    TAILQ_ENTRY(stream) link;
+} stream;
 
 struct thread {
     struct equeue *eq;
@@ -261,13 +193,11 @@ struct thread {
     int pfd[2];
     struct async_io *aio;
     struct edata ev[2];
-    //struct equeue *eq;
     struct server *srv;
-    struct connect *conn;
-    struct connect *conn_head;
-    struct connect *conn_tail;
-    char cb;
+    struct connection *conn;
+
     SIMPLEQ_HEAD(L_map, lua_state_map) L_map;
+    TAILQ_HEAD(conn, connection) conn_t;
 };
 
 struct server {
@@ -275,7 +205,6 @@ struct server {
     int fd;
     int ti;
     uintptr_t aid; // fd or signal for aio signaling
-//    struct sigevent se;
     sock_addr sa46;
     char *progname;
     char *pidfile;
@@ -287,11 +216,14 @@ struct server {
     private_key *pkey;
 };
 
-struct connect {
+
+typedef struct connection {
 
     struct edata ev;
 
-    enum conn_state cs;
+    enum io_state cs;
+
+    //struct active_state as;
 
     int fd;
     struct sockaddr_storage ss;
@@ -299,25 +231,17 @@ struct connect {
     long timestamp;
     struct thread* thr;
 
-    struct connect* next_actv;
-    struct connect* prev_actv;
-
     struct stream *strm;
-    struct stream *strm_head;
-    struct stream *strm_tail; //tail might not be needed
 
+    int http_error;
     enum http_protocol http_protocol;
     char* protocol;
 
-    int keep_alive;
     int conn_close;
     int upgrade;
 
-    int mime_flag;
-    int http_11; // might not be neeeded
-
     char* buf;
-    char rbuf[BUFFER_SIZE * 10];
+    char rbuf[BUFFER_SIZE];
     size_t rlen;
 
     // ssl
@@ -327,13 +251,17 @@ struct connect {
 
     // HTTP/2
     struct h2_frame h2_frm;
-    struct hpack *encoder;
-    struct hpack *decoder;
-    uint8_t *hb;
+
+    struct hpack_table *hpack_dec;
+    struct hpack_table *hpack_enc;
+    struct hpack_headerblock *hpack_hbd;
+    struct hpack_headerblock *hpack_hbe;
+
+/////////////////////////////////////////
 
     enum h2_state h2_state;
-    enum h2_error_codes h2_err;
     struct h2_settings h2_set;
+    enum h2_error h2_error;
 
     int cont_sid;
     int prom_sid;
@@ -344,11 +272,13 @@ struct connect {
 
     int h2_preface;
     int hs;
+    uint32_t h_sid;
 
-    int cdbg;
-};
+    int not_found;
 
-
+    TAILQ_HEAD(strm, stream) strm_t;
+    TAILQ_ENTRY(connection) link;
+} connection;
 
 
 //log.c
@@ -364,22 +294,17 @@ double get_elapsed_time(struct timespec *before, struct timespec *after);
 
 //httpd-lua.c
 int lua_map_create(struct thread*, struct l_map*);
-//int lua_create_smap lua_create_states
-int process_lua(struct stream*);
 static int register_handler(lua_State*);
-struct lua_handler* find_handler(struct lua_state_map*, char*);
+struct lua_handler* find_handler(struct lua_state_map*, char*, char*);
 void stack_dump(lua_State *L, const char *stackname);
 int lev_read(struct stream* strm, char* buf, int len);
 int lev_write(struct stream* strm, char* buf, int len);
-void lthread_remove(lua_State *, lua_State **);
 int lua_run(lua_State *, lua_State *, int);
 void lh_aio_dispatch(struct aio_data *aio_d);
 
 
 // httpd-aio
-//void lh_aio_ready(uintptr_t aid, struct thread *thr);
-//void lh_aio_ready(uintptr_t aid, void *thr);
-//void lh_aio_ready(int fd, void *thr);
+
 void thread_wakeup(struct edata *ev);
 
 //httpd.c
@@ -387,60 +312,30 @@ char* e_strdup(const char* oldstr);
 void* serve(void *thread);
 
 int new_conn(struct thread* thr);
-void del_conn(struct connect* conn);
+void del_conn(struct connection *conn);
 
 void conntab_create(struct edata *ev);
-void conntab_remove(struct connect*);
-void conntab_update(struct connect*);
+void conntab_remove(struct connection *);
+void conntab_update(struct connection *);
 
 void conn_read(struct edata *ev);
 void conn_write(struct edata *ev);
 
-void http_write_header(struct connect* conn);
-void http_write_body(struct connect* conn);
+void http_write_header(struct connection *conn);
+void http_write_body(struct connection *conn);
 
-void https_io(void* edata);
+void conn_io(struct connection *conn);
 void conn_resume(struct stream*);
 void rw_io(void* edata);
 void reset_headers(struct stream*);
 
 //int http2_write(struct connect*, char*, int);
-int http2_read(struct connect*, char*, int);
+//int http2_read(struct connection *, char*, int);
 
-struct stream* new_strm(struct connect* conn, uint32_t sid);
+struct stream* new_strm(struct connection *conn, uint32_t sid);
+struct stream* h2_find_stream(struct connection *conn, uint32_t h2_sid);
 
-//event.c
-//int equeue_init(struct equeue**);
-//int equeue_add(struct equeue*, int, void*);
-//int equeue_add(struct equeue*, int, short, void*);
-//int equeue_poll(struct equeue *eq, int tv);
-
-//static double ts_to_tv(struct timespec* ts);
 static long ts_to_tv(struct timespec* ts);
 struct timespec tv_to_ts(unsigned long tv);
 
 #endif
-/*
-
-struct connect {
-    int sfd;
-    struct sockaddr_in6 sa;
-    enum {FREE, RECV, SEND} status;
-    double timestamp;
-    header *request;
-    header *response;
-};
-
-
-void fd_send(struct connect*);
-void fd_recv(struct connect*);
-double ts_to_tv(struct timespec* ts);
-struct timespec* tv_to_ts(double tv);
-
-*/
-/* event.c */
-/*
-int equeue_init(void);
-void equeue_edit(int id, void *conn);
-void equeue_exec(int id, double tval);
-*/
